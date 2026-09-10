0188ee865e9815d2f9273ef571634533c48f5180
7oda <mlieegy@gmail.com>
2026-09-10 19:01:24 +0300

    Question02

--- C#Basics05/Program.cs
+++ C#Basics05/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace C_Basics05
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace C_Basics05
 {
     internal class Program
     {
@@ -8,9 +10,22 @@
             #region Question01
             ////Create a one-dimensional array double[] prices with the values 25.5, 40.0, 33.75. Print
             ////the second price(index 1).
-            
+
             //double[] prices = [ 25.5, 40.0, 33.75 ];
             //Console.WriteLine(prices[1]);
+
+            #endregion
+
+            #region Question02
+            //Create a 2x2 multidimensional array int[,] shelfCopies where shelf 0 has 3, 5 copies and shelf 1 has 1, 4 copies.
+            //Print the number of copies on shelf 1, slot 0
+
+            //int[,] shelfCopies = new int[2, 2]
+            //{
+            //    { 3, 5 },
+            //    { 1, 4 }
+            //};
+            //Console.WriteLine(shelfCopies[1, 0]);
 
             #endregion
             #endregion
